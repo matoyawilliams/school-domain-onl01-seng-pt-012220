39215afb6e429fa33c427561b409a9b2f5ddc903
Matoya Williams <matoyawilliams@yahoo.com>
2020-02-20 03:47:04 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,25 +1,28 @@
-def sort 
-  nu_hash = {}
-  roster.each do |x, y| 
-    nu_hash[x] = y.sort 
-  end 
-  nu_hash
+class School
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def roster
+    @roster
+  end
+
+  def add_student(name, grade)
+    if @roster[grade] != nil
+      @roster[grade] << name
+    else
+      @roster[grade] = [name]
+    end
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.each do |key, value|
+      value.sort!
+    end
+  end
 end 
-  
-end 
-
-
-
-school = School.new("Bayside High School")
-school.roster
-school.add_student("Zach Morris", 9)
-school.add_student("Moshe Zeus", 9)
-school.add_student("Onkelos Octavius", 9)
-school.roster
-school.add_student("Sugar Zaza", 5)
-school.roster
-school.add_student("Theo Madus", 5)
-school.add_student("Abbah Zabbah", 2)
-school.roster
-school.grade(9)
-school.sort
